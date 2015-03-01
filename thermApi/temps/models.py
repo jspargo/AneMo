9d3fe4d542d45906b41281e7a9b55c46ddd5bac0
Jack Spargo <jack.spargo@gmail.com>
2015-03-01 16:47:29 +0000

    adding model for temps

--- thermApi/temps/models.py
+++ thermApi/temps/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class currentTemp(models.Model):
+    temp_timestamp = models.DateTimeField('date recorded')
+    temp_val = models.IntegerField(default=0)
