3c2d017dec805805548a3d9f6564b293cf5459b4
Jack Spargo <jack.spargo@gmail.com>
2015-03-01 23:27:00 +0000

    updating api types

--- thermApi/temps/models.py
+++ thermApi/temps/models.py
@@ -1,7 +1,10 @@
+import datetime
+
+from django.utils import timezone
 from django.db import models
 
 # Create your models here.
 
 class currentTemp(models.Model):
     temp_timestamp = models.DateTimeField('date recorded')
-    temp_val = models.DecimalField(..., max_digits=5, decimal_places=2)
+    temp_val = models.DecimalField(default=0, max_digits=5, decimal_places=2)
