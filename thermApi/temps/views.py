81f394a81a9d81bca371134a3eefc2b216c849a5
Jack Spargo <jspargo@deloitte.co.uk>
2015-03-17 19:35:05 +0000

    update view for currentTemp

--- thermApi/temps/views.py
+++ thermApi/temps/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from temps.models import currentTemp
 
-# Create your views here.
+
+def index(request):
+    latest_temp = currentTemp.objects[:5]
+    context = {'latest_temp': latest_temp}
+    return render(request, 'temps/index.html', context)
