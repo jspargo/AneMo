e5c5cd2483a70220f546c623e4d39fd6d9256d5b
Jack Spargo <jspargo@deloitte.co.uk>
2015-03-17 20:54:02 +0000

    starting JSON object

--- thermApi/temps/views.py
+++ thermApi/temps/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from temps.models import currentTemp
+from django.core import serializers
 
 
 def index(request):
     latest_temp = currentTemp.objects.order_by('-temp_timestamp')[:3]
-    context = {'latest_temp': latest_temp}
-    return render(request, 'temps/index.html', context)
+    #context = {'latest_temp': latest_temp}
+    #return render(request, 'temps/index.html', context)
+    serialized_obj = serializers.serialize('json', [ latest_temp, ])
+    return serialized_obj
 
 def current(request):
     latest_temp = currentTemp.objects.order_by('-temp_timestamp')[:1]
