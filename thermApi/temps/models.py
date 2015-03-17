04ecf03c026b0e05385adc7e39444bf73ae800ba
Jack Spargo <jspargo@deloitte.co.uk>
2015-03-17 19:22:20 +0000

    indentation!

--- thermApi/temps/models.py
+++ thermApi/temps/models.py
@@ -10,5 +10,5 @@ class currentTemp(models.Model):
     temp_timestamp = models.DateTimeField('date recorded')
     temp_val = models.DecimalField(default=0, max_digits=5, decimal_places=2)
 
-	def __unicode__(self):
-		return "{:.2f}".temp_val
+    def __unicode__(self):
+        return "{:.2f}".temp_val
