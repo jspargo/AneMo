de70a731eba9ecf0914e161370687b9e797a4668
Jack Spargo <jspargo@deloitte.co.uk>
2015-03-17 19:50:00 +0000

    can we have a view, please?

--- thermApi/temps/models.py
+++ thermApi/temps/models.py
@@ -11,5 +11,11 @@ class currentTemp(models.Model):
     temp_val = models.DecimalField(default=0, max_digits=5, decimal_places=2)
 
     def __unicode__(self):
-        temp_string = unicode(self.temp_val)
+        temp_string = str(self.temp_val)
         return temp_string
+    
+    def was_recorded_recently(self):
+        return self.temp_timestamp >= timezone.now() - datetime.timedelta(days=1)
+    was_recorded_recently.admin_order_field = 'temp_timestamp'
+    was_recorded_recently.boolean = True
+    was_recorded_recently.short_description = 'Temp recorded recently?'
--- thermApi/temps/views.py
+++ thermApi/temps/views.py
@@ -3,6 +3,6 @@ from temps.models import currentTemp
 
 
 def index(request):
-    latest_temp = currentTemp.objects[:5]
+    latest_temp = currentTemp.objects.order_by('-temp_timestamp')[:3]
     context = {'latest_temp': latest_temp}
     return render(request, 'temps/index.html', context)
