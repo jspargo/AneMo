1af1dc0ca0103f7c4232200700a6010231238793
Jack Spargo <jspargo@deloitte.co.uk>
2015-03-17 19:18:41 +0000

    adding admin val

--- thermApi/temps/models.py
+++ thermApi/temps/models.py
@@ -9,3 +9,6 @@ from temps import thermostat
 class currentTemp(models.Model):
     temp_timestamp = models.DateTimeField('date recorded')
     temp_val = models.DecimalField(default=0, max_digits=5, decimal_places=2)
+
+	def __unicode__(self):
+        return "{:.2f}".temp_val
