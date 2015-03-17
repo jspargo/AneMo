4937030fb98f1b21364c94ea6ee6130da4845418
Jack Spargo <jspargo@deloitte.co.uk>
2015-03-17 19:11:26 +0000

    Revert "urgh..."
    This reverts commit 021fec73e1db4f111e39b80835d088d05af32ccd.

---
+++ thermApi/temps/models.py
@@ -0,0 +1,11 @@
+import datetime
+
+from django.utils import timezone
+from django.db import models
+from temps import thermostat
+
+# Create your models here.
+
+class currentTemp(models.Model):
+    temp_timestamp = models.DateTimeField('date recorded')
+    temp_val = models.DecimalField(default=0, max_digits=5, decimal_places=2)
