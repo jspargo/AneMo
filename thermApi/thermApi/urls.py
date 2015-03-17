c522414eb1d6f9d102753d8b7ced5fa3f97be958
Jack Spargo <jspargo@deloitte.co.uk>
2015-03-17 19:36:32 +0000

    is this a better url method?

--- thermApi/thermApi/urls.py
+++ thermApi/thermApi/urls.py
@@ -7,5 +7,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^temps/', include('temps.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'/admin/', include(admin.site.urls)),
 )
