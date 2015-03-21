00593f8504ddd8ea2eaf27149f8a14f51cca6d14
jspargo <jack.spargo@gmail.com>
2015-03-21 19:41:48 +0000

    tweaking response from flask to django

--- thermo/app.py
+++ thermo/app.py
@@ -21,9 +21,10 @@ def temp():
     json_response = jsonify(data)
     headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
     r = requests.post(django_url, data=data, headers=headers)
-    if r.raise_for_status() is None:
-        return '<em>Status 200:</em> ' + r
-    return 'An error has occurred'
+    #if r.raise_for_status() is None:
+    #    return '<em>Status 200:</em> ' + r
+    #return 'An error has occurred'
+    return 'Finished'
 
 def shutdown():
     currentTemp().keyboard_interupt()
