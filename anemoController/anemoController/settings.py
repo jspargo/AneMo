d6ac580b0c1ddd1cafed3e69e023892210e9ccfe
Jack Spargo <jack@hockleyspargo.co.uk>
2015-03-28 16:07:54 +0000

    starting REST framework setup

--- anemoController/anemoController/settings.py
+++ anemoController/anemoController/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'temps',
 )
 
@@ -86,3 +87,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# REST API configuration
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions, or allow read-only access for 
+    # unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
+
--- anemoController/anemoController/urls.py
+++ anemoController/anemoController/urls.py
@@ -1,5 +1,9 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
+from django.conf.urls import patterns, include, url 
+from django.contrib import admin 
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
+
+
 
 urlpatterns = patterns('',
     # Examples:
@@ -8,4 +12,5 @@ urlpatterns = patterns('',
 
     url(r'^temps/', include('temps.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 )
