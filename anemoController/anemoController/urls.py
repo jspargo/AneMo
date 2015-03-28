4228eccfd74e3df6173eaf559f5fb9659cbcb4f2
Jack Spargo <jack.spargo@gmail.com>
2015-03-28 17:37:50 +0000

    adding temp serialiser for Django REST framework (i.e. more 'out of the box' API response)

--- anemoController/anemoController/urls.py
+++ anemoController/anemoController/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url 
 from django.contrib import admin 
 from django.contrib.auth.models import User
+from temps.models import RecordedTemp, SetTemp
 from rest_framework import routers, serializers, viewsets
 
 # Serializers define the API representation.
@@ -14,9 +15,22 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+
+class TempSerializer(serializers.HyperlinkedModelSerializer):
+	class Meta:
+		model = RecordedTemp
+		fields = ('recorded_temp', 'recorded_date')
+
+
+class TempViewSet(viewsets.ModelViewSet):
+	queryset = RecordedTemp.objects.all()
+	serializer_class = TempSerializer
+
+
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
+router.register(r'temp', TempViewSet)
 
 urlpatterns = patterns('',
 	url(r'^', include(router.urls)),
