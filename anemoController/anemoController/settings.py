022c1511ada9ae0df106f46956842198495c023b
jspargo <jack.spargo@gmail.com>
2015-11-28 16:56:10 +0000

    changing to OS variable

--- anemoController/anemoController/settings.py
+++ anemoController/anemoController/settings.py
@@ -10,19 +10,15 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-import ConfigParser
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
-# Use config file to read in keys and config settings
-config = ConfigParser.ConfigParser()
-config.read('config.cfg')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config.get('djangoSettings', 'secret_key')
+# We have set the key as environment variable in OS
+SECRET_KEY = os.environ['DJANGOKEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
