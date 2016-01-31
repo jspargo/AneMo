0785f6a9e7442811cf54f2a135b1cd020b545b5c
jspargo <jack.spargo@gmail.com>
2016-01-31 11:39:46 +0000

    python code quality...

--- anemoController/anemoController/urls.py
+++ anemoController/anemoController/urls.py
@@ -1,14 +1,16 @@
-from django.conf.urls import patterns, include, url 
-from django.contrib import admin 
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
 from django.contrib.auth.models import User
 from rest_framework import routers, serializers, viewsets
-from temps.restApi import TempSerializer, TempViewSet, GetLatestViewSet
+from temps.restApi import TempViewSet, GetLatestViewSet
+
 
 # Serializers define the API representation.
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ('url', 'username', 'email', 'is_staff')
+
 
 # ViewSets define the view behavior.
 class UserViewSet(viewsets.ModelViewSet):
@@ -23,8 +25,9 @@ router.register(r'temp', TempViewSet)
 router.register(r'getlatest', GetLatestViewSet)
 
 urlpatterns = patterns('',
-	url(r'^', include(router.urls)),
-    url(r'^temps/', include('temps.urls')),
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-)
+                       url(r'^', include(router.urls)),
+                       url(r'^temps/', include('temps.urls')),
+                       url(r'^getlates/', include('temps.urls')),
+                       url(r'^admin/', include(admin.site.urls)),
+                       url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+                       )
--- anemoController/temps/models.py
+++ anemoController/temps/models.py
@@ -4,10 +4,11 @@ import json, datetime
 
 # Create your models here.
 
+
 class TempManager(models.Manager):
-	def current_temp_within_range(self):
-		#TODO: here we will compare latest recorded temp with set temps
-	  	return True
+    def current_temp_within_range(self):
+        #TODO: here we will compare latest recorded temp with set temps
+        return True
 
 
 class RecordedTemp(models.Model):
@@ -16,9 +17,12 @@ class RecordedTemp(models.Model):
 
     objects = TempManager()
 
+    class Meta:
+        get_latest_by = 'recorded_date'
+
     def __unicode__(self):
-    	temp = float(self.recorded_temp)
-    	date = str(self.recorded_date)
+        temp = float(self.recorded_temp)
+        date = str(self.recorded_date)
         return u'%f -- %s' % (temp, date)
 
     def was_recorded_today(self):
@@ -31,13 +35,13 @@ class SetTemp(models.Model):
     set_date = models.DateTimeField('date set')
 
     def __unicode__(self):
-    	temp_low = float(self.set_temp_low)
-    	temp_high = float(self.set_temp_high)
-    	date = str(self.set_date)
+        temp_low = float(self.set_temp_low)
+        temp_high = float(self.set_temp_high)
+        date = str(self.set_date)
         return u'%f to %f -- %s' % (temp_low, temp_high, date)
 
 
 class TimeConversion(str):
-	def __str__(self):
-		output = datetime.datetime.strptime(self, '%a, %d %b %Y %H:%M:%S GMT').strftime('%Y-%m-%d')
-		return output
+    def __str__(self):
+        output = datetime.datetime.strptime(self, '%a, %d %b %Y %H:%M:%S GMT').strftime('%Y-%m-%d')
+        return output
