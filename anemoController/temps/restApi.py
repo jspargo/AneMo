dbbc2d9bc4effccb9cbe68d807b337640ccf2391
jspargo <jack.spargo@gmail.com>
2016-01-31 11:58:43 +0000

    sorting more python quality issues and trying to add get_latest function

--- anemoController/temps/restApi.py
+++ anemoController/temps/restApi.py
@@ -3,16 +3,22 @@ from rest_framework import routers, serializers, viewsets
 
 
 class TempSerializer(serializers.HyperlinkedModelSerializer):
-	class Meta:
-		model = RecordedTemp
-		fields = ('recorded_temp', 'recorded_date')
+    class Meta:
+        model = RecordedTemp
+        fields = ('recorded_temp', 'recorded_date')
 
 
 class TempViewSet(viewsets.ModelViewSet):
-	queryset = RecordedTemp.objects.all()
-	serializer_class = TempSerializer
+    queryset = RecordedTemp.objects.all()
+    serializer_class = TempSerializer
+
+
+class GetLatestSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = RecordedTemp
+        fields = ('recorded_temp', 'recorded_date')
 
 
 class GetLatestViewSet(viewsets.ModelViewSet):
-	queryset = RecordedTemp.objects.all()
-	serializer_class = TempSerializer
+    queryset = RecordedTemp.objects.latest('recorded_date')
+    serializer_class = GetLatestSerializer
