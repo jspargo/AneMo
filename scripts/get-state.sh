9c43d0f0f691c6d6959bfbdb74dc77e6baa6d2d0
Jack Spargo <jack.spargo@gmail.com>
2017-02-18 13:56:08 +0000

    Updating node in script

--- scripts/get-state.sh
+++ scripts/get-state.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 export PATH="/usr/local/bin/:$PATH"
-node /home/pi/Developer/AneMo/receiver/lib/index.js >> /home/pi/Developer/AneMo/receiver/log/cron.log
+/usr/local/bin/node /home/pi/Developer/AneMo/receiver/lib/index.js >> /home/pi/Developer/AneMo/receiver/log/cron.log
