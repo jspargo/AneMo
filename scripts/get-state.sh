544ce9cf7861b96e2a8457b8aa93b2417ac88e80
Jack Spargo <jack.spargo@gmail.com>
2017-02-18 14:02:25 +0000

    adding env vars script

--- scripts/get-state.sh
+++ scripts/get-state.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 export PATH="/usr/local/bin/:$PATH"
+/home/pi/set-env-vars.sh
 /usr/local/bin/node /home/pi/Developer/AneMo/receiver/lib/index.js >> /home/pi/Developer/AneMo/receiver/log/cron.log
