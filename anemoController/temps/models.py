08629950d9c7e2d5f5dab7c52787a4e43bada442
Jack Spargo <jack.spargo@gmail.com>
2017-03-02 21:21:11 +0000

    Finishing up the time-based logic

--- anemoController/temps/models.py
+++ anemoController/temps/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 import json, datetime
 import logging
@@ -52,13 +53,47 @@ class TempManager(models.Model):
     state_date = models.DateTimeField('state date')
 
     def calculate_state_logic(self):
-        "Using Temps and Dates, establish logic state to return"
+        "Using Temps and Dates/Times, establish logic state to return"
         if self.recorded_temp < self.requested_temp:
-            logging.warning('%f is less than %f - heating state set to ON', self.recorded_temp, self.requested_temp)
-            return True
+            if self.calculate_time_range():
+                logging.warning('Time is within range')
+                logging.warning('%f is less than %f - heating state set to ON', self.recorded_temp, self.requested_temp)
+                return True
+            else:
+                logging.warning('Time is outside range')
+                return False
         else:
             logging.warning('%f is greater than %f - heating state set to OFF', self.recorded_temp, self.requested_temp)
             return False
+
+    def calculate_time_range(self):
+        now = timezone.now()
+        setTime = SetTimes.objects.latest()
+
+        timeNow = now.time()
+        firstTime = setTime.set_first_time.time()
+        secondTime = setTime.set_second_time.time()
+        thirdTime = setTime.set_third_time.time()
+        fourthTime = setTime.set_fourth_time.time()
+
+        if timeNow < firstTime:
+            logging.warning('Before first: %s is before %s', timeNow, firstTime)
+            return False
+        elif timeNow < secondTime:
+            logging.warning('Within first: %s is before %s', timeNow, secondTime)
+            return True
+        elif timeNow < thirdTime:
+            logging.warning('Before second: %s is before %s', timeNow, thirdTime)
+            return False
+        elif timeNow < fourthTime:
+            logging.warning('Within second: %s is before %s', timeNow, fourthTime)
+            return True
+        elif timeNow > fourthTime:
+            logging.warning('After second: %s is after %s', timeNow, fourthTime)
+            return False
+        else:
+            logging.error('Error')
+            return
 
     def _get_state_to_return(self):
         "Returns state (overridden or logic)"
@@ -104,7 +139,9 @@ class SetTimes(models.Model):
         ordering = ['-time_set_date']
 
     def __unicode__(self):
-        temp_low = float(self.set_temp_low)
-        temp_high = float(self.set_temp_high)
-        date = str(self.set_date)
+        first_time = str(self.set_first_time)
+        second_time = str(self.set_second_time)
+        third_time = str(self.set_third_time)
+        fourth_time = str(self.set_fourth_time)
+        date = str(self.time_set_date)
         return u'%f to %f -- %s' % (temp_low, temp_high, date)
